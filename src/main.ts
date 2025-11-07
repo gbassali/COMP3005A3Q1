b69dfee1bda21ead44077af9d908dcb399b31894
gbassali <gurleenbassali@gmail.com>
2025-11-06 20:07:54 -0500

    crud functions

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,1 @@
 import { dbPool } from "./db";
-
-export async function getAllStudents() {
-
-}
-
-export async function addStudent(first_name: string, last_name: string, email: string, enrollment_date: string|null) {
-
-
-}
-
-export async function updateStudentEmail(student_id: number, new_email: string) {
-
-}
-
-export async function deleteStudent(student_id: number) {
-    
-}
--- src/student.ts
+++ src/student.ts
@@ -1,1 +1,30 @@
 import { dbPool } from "./db";
+
+export async function getAllStudents() {
+    const { rows } = await dbPool.query('SELECT * FROM students');
+    return rows;
+}
+
+export async function addStudent(first_name: string, last_name: string, email: string, enrollment_date: string|null) {
+    const { rows } = await dbPool.query(
+        'INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ($1, $2, $3, $4) RETURNING *',
+        [first_name, last_name, email, enrollment_date]
+    );
+    return rows[0];
+}
+
+export async function updateStudentEmail(student_id: number, new_email: string) {
+    const { rows } = await dbPool.query(
+        `UPDATE students SET email = $1 WHERE student_id = $2 RETURNING *;`,
+        [new_email, student_id]
+    );
+    return rows[0];
+}
+
+export async function deleteStudent(student_id: number) {
+    const { rows } = await dbPool.query(
+        `DELETE FROM students WHERE student_id = $1 RETURNING *;`,
+        [student_id]
+    );
+    return rows[0];
+}
