d78c03e3442c07b910e3d8e37f55052dda69b6fd
gbassali <gurleenbassali@gmail.com>
2025-11-06 19:41:14 -0500

    first commit

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg"
+
+export const dbPool = new Pool({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT ?? 5432),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+});
